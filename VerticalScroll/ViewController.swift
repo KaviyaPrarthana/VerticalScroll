b0df75b6d69d0a1c1fb36c08b4744e9eeb9757e8
KaviyaPrarthana <prarthanamanokaran@gmail.com>
2021-01-21 13:09:43 +0530

    Initial Commit

---
+++ VerticalScroll/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VerticalScroll
+//
+//  Created by Kaviya Prarthana on 21/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
