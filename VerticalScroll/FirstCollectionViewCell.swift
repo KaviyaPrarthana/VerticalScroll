488fba920f8b8ab523ca872b2a2fd24bc38da6e5
KaviyaPrarthana <prarthanamanokaran@gmail.com>
2021-01-21 13:19:06 +0530

    Initial commit with vertical swipe

---
+++ VerticalScroll/FirstCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  FirstCollectionViewCell.swift
+//  VerticalScroll
+//
+//  Created by Kaviya Prarthana on 21/01/21.
+//
+
+import UIKit
+
+class FirstCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var poster: UIImageView!
+
+    //sample
+}
--- VerticalScroll/ViewController.swift
+++ VerticalScroll/ViewController.swift
@@ -7,13 +7,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+struct Content {
+    var poster : String
+}
 
+class ViewController: UIViewController,UICollectionViewDataSource,UICollectionViewDelegate,UICollectionViewDelegateFlowLayout {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    var details = [Content(poster: "album1"),Content(poster: "album2")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.initScreen()
     }
-
+    
+    func initScreen(){
+        self.collectionView.dataSource = self
+        DispatchQueue.main.async {
+            self.collectionView.reloadData()
+        }
+    }
+//MARK:- CollectionView DataSource
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.details.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)as! FirstCollectionViewCell
+        cell.poster.image = UIImage(named: self.details[indexPath.row].poster)
+        return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width:self.collectionView.frame.width, height: self.collectionView.frame.height)
+    }
+    
 
 }
+
+
+
+
 
